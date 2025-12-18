094f335db72d95b8c341f3dbca633d19eaaf9d20
YC Chen <ycchen@YCdeMacBook-Air.local>
2025-12-18 22:27:34 +0800

    feat: integrate PWA with existing Vercel config

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,20 @@
-// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { AuthProvider } from './contexts/AuthContext'
-import { LedgerProvider } from './contexts/LedgerContext' // 新增
+import { LedgerProvider } from './contexts/LedgerContext'
+// [PWA] 引入註冊模組
+import { registerSW } from 'virtual:pwa-register'
+
+// [PWA] 註冊 Service Worker
+// 這段程式碼只會在生產環境 (build 後) 生效，讓瀏覽器開始快取
+const updateSW = registerSW({
+  onNeedRefresh() {},
+  onOfflineReady() {
+    console.log('App ready to work offline')
+  },
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
