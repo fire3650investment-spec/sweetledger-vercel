03fe33ff3a27f531a59dc6f981f059191a5b0554
YC Chen <ycchen@YCdeMacBook-Air.local>
2025-12-18 22:23:02 +0800

    init: project rescue point

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,17 @@
+// src/main.jsx
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
-// [PWA] 引入註冊模組
-import { registerSW } from 'virtual:pwa-register'
-
-// [PWA] 註冊 Service Worker
-// 只有在生產環境 (build 後) 才會真正執行，配合 vite.config.js 的 devOptions: false
-const updateSW = registerSW({
-  onNeedRefresh() {
-    // 雖然我們開了 autoUpdate，但如果有特殊狀況，這裡可以處理
-    // 目前我們信任 autoUpdate 會自動處理
-  },
-  onOfflineReady() {
-    console.log('App ready to work offline')
-  },
-})
+import { AuthProvider } from './contexts/AuthContext'
+import { LedgerProvider } from './contexts/LedgerContext' // 新增
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <LedgerProvider> {/* 包在 AuthProvider 裡面，因為 Ledger 可能需要 User 資訊 */}
+        <App />
+      </LedgerProvider>
+    </AuthProvider>
   </React.StrictMode>,
 )
